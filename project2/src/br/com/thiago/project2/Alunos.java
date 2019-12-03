da089ec166d6617c3cfa753987eafc69e454f8d1
tmilitino <tmilitino.tm@gmail.com>
2019-12-03 08:13:34 -0300

    projeto 2
    falta output

---
+++ project2/src/br/com/thiago/project2/Alunos.java
@@ -0,0 +1,55 @@
+package br.com.thiago.project2;
+
+public final class Alunos {
+	
+	String[] nomes;
+	
+	
+	double [][] notas;
+	
+	public void SetNomes(String[] nomes) {
+		this.nomes =nomes;
+	}
+	
+	public String[] GetNomes() {
+		return nomes;
+		
+	}
+	
+	public void GetNotas(double [][] notas) {
+		this.notas = notas;
+	}
+	
+	
+	public double[][] GetNotas(){
+		return notas;
+	}
+	
+	public double nota_media(double[] notas) {
+	double media = 0;
+		for(double nota: notas) {
+			media = media + nota;
+		}
+		
+		return media/notas.length;
+		
+	}
+	
+	public String situacao(double media) {
+		
+		if (media<4.00) {
+			return "Reprovado";
+		}
+		else if(media>=4.00 && media<7.00) {
+			return "Final";
+		}
+		else if(media>=7.00) {
+			return "Aprovado";
+		}
+		return "";
+		
+	}
+	
+	
+
+}
